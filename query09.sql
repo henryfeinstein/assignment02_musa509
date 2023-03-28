0473401c11bdb1073deec305de25d9a829605300
henryfeinstein <98499896+henryfeinstein@users.noreply.github.com>
2023-03-28 14:38:48 -0400

    query 9

---
+++ query09.sql
@@ -0,0 +1,13 @@
+-- filter parcels to just meyerson address
+-- spatial join w block group and pull geoid
+
+with
+
+meyerson_parcel as (
+    select * from phl.pwd_parcels
+    where address = '220-30 S 34TH ST'
+)
+
+select geoid as geo_id from census.blockgroups_2020
+join meyerson_parcel
+on st_intersects(census.blockgroups_2020.geog, meyerson_parcel.geog)
